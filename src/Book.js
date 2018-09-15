5c421ccd49a6eae7b5b01c11ae5f3feda2353ad4
Nikiya Simpson <nikiyasimpson@gmail.com>
2018-09-14 21:49:46 -0500

    initial components'

---
+++ src/Book.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+
+class Book extends Component {
+  
+  render() {
+    console.log('Props',this.props);
+     return (
+       <div className="book">
+
+        </div>
+       /*<div className="book">
+         <div className="book-top">
+           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: {this.props.coverImage} }}></div>
+           <div className="book-shelf-changer">
+             <select>
+               <option value="move" disabled>Move to...</option>
+               <option value="currentlyReading">Currently Reading</option>
+               <option value="wantToRead">Want to Read</option>
+               <option value="read">Read</option>
+               <option value="none">None</option>
+             </select>
+           </div>
+         </div>
+         <div className="book-title">{this.props.title}</div>
+         <div className="book-authors">{this.props.author}</div>
+       </div>*/
+     )
+   }
+ }
+
+ export default Book
---
+++ src/BookList.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import BookShelf from './BookShelf'
+//import { Link } from 'react-router-dom'
+
+
+      function isCurrentlyReading(book){
+        if (book.readStatus === 'Currently Reading')
+        return true;
+  }
+
+class BookList extends Component {
+
+
+
+  render() {
+
+      console.log('Props',this.props);
+      return (
+        <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                      {this.props.books.filter(isCurrentlyReading).map((book) => (
+                          <li key={book.id} className=''>
+                          <div className="book">
+                            <div className="book-top">
+                              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.coverImage})` }}></div>
+                              <div className="book-shelf-changer">
+                                <select>
+                                  <option value="move" disabled>Move to...</option>
+                                  <option value="currentlyReading">Currently Reading</option>
+                                  <option value="wantToRead">Want to Read</option>
+                                  <option value="read">Read</option>
+                                  <option value="none">None</option>
+                                </select>
+                              </div>
+                            </div>
+                            <div className="book-title">{book.title}</div>
+                            <div className="book-authors">{book.author}</div>
+                          </div>
+                        </li>
+                      ))}
+
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+
+                </ol>
+              </div>
+            </div>
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+
+                </ol>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="open-search">
+          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+        </div>
+        </div>
+      )
+      }
+
+}
+
+export default BookList
